078a644a8d792654f9b63d13067f575a04f0ea44
Scanholato <scanholato.murilo@gmail.com>
2025-08-29 10:58:33 -0300

    Atualização

--- atividade07/questao04.py
+++ atividade07/questao04.py
@@ -1,17 +1,14 @@
 import json
 
-# Dados
 pessoa = {
     "nome": "Murilo",
     "idade": 30,
     "cidade": "Águas de São Pedro"
 }
 
-# Escrevendo JSON
 with open("pessoa.json", "w", encoding="utf-8") as arquivo:
     json.dump(pessoa, arquivo, indent=4, ensure_ascii=False)
 
-# Lendo JSON
 with open("pessoa.json", "r", encoding="utf-8") as arquivo:
     dados = json.load(arquivo)
     print(f"Nome: {dados['nome']}, Idade: {dados['idade']}, Cidade: {dados['cidade']}")
